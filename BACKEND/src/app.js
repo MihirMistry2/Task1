d209cf050ccfc2b4961d8085c539dfff48ce9fe7
MihirMistry2 <mihirmistry82@gmail.com>
2021-03-18 20:11:15 +0530

    add initial routes, add CORS handler request and set static request folder(public)

---
+++ BACKEND/src/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+
+const routes = require('./routers/routes');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'PUT, POST, GET, DELETE, OPTIONS'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header(
+            'Access-Control-Allow-Methods',
+            'PUT',
+            'POST',
+            'PATCH',
+            'DELETE',
+            'GET',
+            'POST'
+        );
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
+app.use('/api/companys', routes);
+
+module.exports = app;
