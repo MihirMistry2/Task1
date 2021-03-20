ae14edd5a764e656606456abd724f3b20a97b777
MihirMistry2 <mihirmistry82@gmail.com>
2021-03-20 11:00:53 +0530

    path change list to lists

--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
   { path: 'lists', component: CompanysComponent },
-  { path: 'list/:id', component: FormComponent },
+  { path: 'lists/:id', component: FormComponent },
   { path: '', redirectTo: '/lists', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
