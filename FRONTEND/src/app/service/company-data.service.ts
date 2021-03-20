3ab561985a1808b88f54af4b163167fce184f497
MihirMistry2 <mihirmistry82@gmail.com>
2021-03-20 10:59:39 +0530

    add create and update http request

--- FRONTEND/src/app/service/company-data.service.ts
+++ FRONTEND/src/app/service/company-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -13,6 +13,14 @@ export class CompanyDataService {
 
   getCompanyById(id: number) {
     return this.http.get<any>(`http://localhost:3200/api/companys/${id}`);
+  }
+
+  createCompany(lists) {
+    return this.http.post(`http://localhost:3200/api/companys/`, lists);
+  }
+
+  updateCompany(id, lists) {
+    return this.http.patch(`http://localhost:3200/api/companys/${id}`, lists);
   }
 
   deleteCompany(id: number) {
